c838b656ddf38b758f167c49d7768655091a3cea
yungstarry <111007711+yungstarry@users.noreply.github.com>
2023-05-03 06:13:05 +0100

    first commit

---
+++ src/contact.js
@@ -0,0 +1,29 @@
+function createContact(){
+    const contact = document.createElement('div')
+    contact.classList.add('contact')
+
+    const phoneNumber = document.createElement('p')
+    phoneNumber.textContent = '📞 123 456 789'
+
+    const address = document.createElement('p')
+    address.textContent = "🏠 Hollywood Boulevard 42, Los Angeles, USA";
+
+    const restaurantLocation = document.createElement('img')
+    restaurantLocation.src = "images/restaurant-location.png";
+    restaurantLocation.alt ='Yomi Kitchen'
+
+    contact.appendChild(phoneNumber)
+    contact.appendChild(address)
+    contact.appendChild(restaurantLocation)
+
+    return contact
+}
+
+
+function loadContact(){
+    const main = document.getElementById('main')
+    main.textContent = ''
+    main.appendChild(createContact())
+}
+
+export default loadContact
